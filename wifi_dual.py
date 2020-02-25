893d48c987d0a914d0d153496a158184a4572b84
Tim <architect314159@gmail.com>
2020-02-25 09:29:59 -0800

    change ssh password and save passwords with username (in base 64)

--- wifi_dual.py
+++ wifi_dual.py
@@ -4,7 +4,7 @@ import serial
 import random
 import time
 import base64
-
+import pexpect
 
 def main():
 	get_user_input()
@@ -18,15 +18,24 @@ def get_user_input():
 		print(color)
 		if color not in ["red", "blue"]:
 			print("please choose either 'red' or 'blue' for your color")
-	save_username(username)
 	send_to_serial(find_device(), color)
+	save_username(username)
 
 
 def send_to_serial(serial_path, team_color):
 	password = get_bad_password()
-	output_string = 'ap -ssid "{}" -password "{}" -channel {}'.format(team_color, password, get_rand_from_seed(1, 11))
+	startAP_string = 'ap -ssid "{}" -password "{}" -channel {}'.format(team_color, password, get_rand_from_seed(1, 11))
+	changeSSH_string = 'passwd {}'.format(password)
 	ser = serial.Serial(serial_path, 115200)
-	ser.write(output_string.encode())
+	ser.write(startAP_string.encode())
+	print("access pont created")
+
+	# change ssh password
+	child = pexpect.spawn('passwd root')
+	child.delaybeforesend = 2
+	child.sendline(password)
+	child.sendline(password)
+	print("changed ssh password")
 
 
 def get_bad_password():
@@ -59,14 +68,16 @@ def find_device():
 		exit()
 
 
-def save_username(username):
-	filename = "usernames.txt"	
+def save_username(username, password):
+	filename = "usernames_w_passwords.txt"	
 	if os.path.exists(filename):
 	    append_write = 'a' # append if already exists
 	else:
 	    append_write = 'w' # make a new file if not
 	with open(filename, append_write) as f:
-		f.write(username)
+		password = base64.b64encode(password.encode('utf-8')).decode('utf-8')
+		line = "{}, {}\n".format(username, password)
+		f.write(line)
 
 
 def get_rand_from_seed(lower, upper):
